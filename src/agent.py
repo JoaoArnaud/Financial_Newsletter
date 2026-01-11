2754aecffccbed297e1b0a2c4d697283fe45fa5c
João André de Medeiros Arnaud <66128302+JoaoArnaud@users.noreply.github.com>
2026-01-11 10:50:19 -0300

    Creating the first agent

--- src/agent.py
+++ src/agent.py
@@ -3,4 +3,12 @@ from agno.tools.tavily import TavilyTools
 from agno.models.openai import OpenAIChat
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv(dotenv_path="/home/joaoa/repos/Financial_Newsletter/.env")
+
+agent = Agent(
+    model=OpenAIChat(id="gpt-4.1-mini"),
+    tools=[TavilyTools()],
+    debug_mode=True
+)
+
+agent.print_response("use your tools to find investment opportunities")
